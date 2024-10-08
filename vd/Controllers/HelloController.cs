0e7393f90206b3f2294a6ff62715bcc0972b0ec3
dhoanIT <dhoan498@gmail.com>
2024-10-08 21:01:41 +0700

    init project

---
+++ vd/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vd.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        //public string Index()
+        //{
+        //    return "Đây là phương thức mặc định của 1 controller bất kì";
+        //}
+        //public string ChaoMung(string ten, int id=1)
+        //{
+        //    return ("Xin Chào " + ten + ".ID: " + id);
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ vd/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MyLink()
+        {
+            ViewBag.Message = "Liên Kết của tôi";
+            return Redirect("https://www.youtube.com/");
+        }
+        public ActionResult Haui()
+        {
+            ViewBag.Message = "Liên Kết của tôi";
+            return Redirect("https://www.facebook.com/");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
